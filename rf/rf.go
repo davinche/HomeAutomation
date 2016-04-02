bf49c9838c4c3f4dfcb5c605b21000dfa61a9f07
Vincent Cheung <vinche7@gmail.com>
2016-04-02 12:36:41 -0700

    refactor rf api

--- rf/rf.go
+++ rf/rf.go
@@ -1,7 +1,11 @@
 package rf
 
 import (
+	"fmt"
+	"homeautomation/apihelpers"
+	"net/http"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
@@ -11,14 +15,13 @@ type Switch struct {
 	On, Off int
 }
 
-// Raw Codes for Wave "on" and "off" -----------------------------------
-var rf0 = "174 522"
-var rf1 = "522 174"
-var rffoot = "174 5916"
-
-// Convert a decimal representation of signal to pilight raw -----------
+// Convert a decimal representation of signal to pilight raw
 func decimalToRaw(n int) string {
+	rf0 := "174 522"
+	rf1 := "522 174"
+	rffoot := "174 5916"
 	bin := make([]string, 0, 25)
+
 	for n > 0 {
 		if n%2 == 0 {
 			bin = append(bin, rf0)
@@ -43,4 +46,61 @@ func SendCode(code int) error {
 	pilightArgs := []string{"-p", "raw", "-c", binCode}
 	cmd := exec.Command("pilight-send", pilightArgs...)
 	return cmd.Run()
+}
+
+// ----------------------------------------------------------------------------
+// API Handler for switches
+// ----------------------------------------------------------------------------
+var switches = []Switch{
+	Switch{On: 1398067, Off: 1398076},
+	Switch{On: 1398211, Off: 1398220},
+	Switch{On: 1398531, Off: 1398540},
+	Switch{On: 1400067, Off: 1400076},
+	Switch{On: 1406211, Off: 1406220},
+}
+
+// SwitchHandler is an HTTP Handler that deals with calls to turn switches on and off.
+// Query Params Supported:
+// state (string): "on | off"
+// switch (int): which switch from the remote to use
+func SwitchHandler(w http.ResponseWriter, r *http.Request) {
+	state := r.URL.Query().Get("state")
+	switchNum := r.URL.Query().Get("switch")
+	if state == "" || switchNum == "" {
+		apihelpers.EncodeError(w, http.StatusBadRequest, "Missing Switch Number or State")
+		return
+	}
+
+	// Get the correspodning switch to turn on
+	intSwitchNum, err := strconv.Atoi(switchNum)
+	if err != nil || intSwitchNum > (len(switches)-1) {
+		apihelpers.EncodeError(w, http.StatusBadRequest, "Invalid Switch Number")
+		return
+	}
+
+	// Get the state to set the switch to
+	state = strings.ToLower(state)
+	if state != "on" && state != "off" {
+		apihelpers.EncodeError(w, http.StatusBadRequest, "Invalid State: must be \"on\" or \"off\"")
+		return
+	}
+
+	// Get the code we want to transmit
+	selectedSwitch := switches[intSwitchNum]
+	code := selectedSwitch.On
+	if state == "off" {
+		code = selectedSwitch.Off
+	}
+
+	// Send the code
+	err = SendCode(code)
+
+	// Handle errors from pilight
+	if err != nil {
+		apihelpers.EncodeError(w, http.StatusInternalServerError, "Unable to send RF Code")
+		return
+	}
+	// Success!
+	success := fmt.Sprintf("Successfully turned switch %d %s", intSwitchNum, state)
+	apihelpers.EncodeJSON(w, http.StatusOK, map[string]string{"message": success})
 }
--- server.go
+++ server.go
@@ -2,72 +2,14 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"homeautomation/apihelpers"
 	"homeautomation/rf"
 	"net/http"
-	"strconv"
-	"strings"
 )
-
-// Map RF Remote Buttons------------------------------------------------
-var switches = []rf.Switch{
-	rf.Switch{On: 1398067, Off: 1398076},
-	rf.Switch{On: 1398211, Off: 1398220},
-	rf.Switch{On: 1398531, Off: 1398540},
-	rf.Switch{On: 1400067, Off: 1400076},
-	rf.Switch{On: 1406211, Off: 1406220},
-}
 
 func main() {
 	port := flag.String("port", "8080", "The port to run the server on")
 	flag.Parse()
 	mux := http.NewServeMux()
-	mux.HandleFunc("/api/switch", switchHandler)
+	mux.HandleFunc("/api/switch", rf.SwitchHandler)
 	http.ListenAndServe(":"+*port, mux)
-}
-
-// ---------------------------------------------------------------------
-// RF Switches API Handler
-// ---------------------------------------------------------------------
-func switchHandler(w http.ResponseWriter, r *http.Request) {
-	state := r.URL.Query().Get("state")
-	switchNum := r.URL.Query().Get("switch")
-	if state == "" || switchNum == "" {
-		apihelpers.EncodeError(w, http.StatusBadRequest, "Missing Switch Number or State")
-		return
-	}
-
-	// Get the correspodning switch to turn on
-	intSwitchNum, err := strconv.Atoi(switchNum)
-	if err != nil || intSwitchNum > (len(switches)-1) {
-		apihelpers.EncodeError(w, http.StatusBadRequest, "Invalid Switch Number")
-		return
-	}
-
-	// Get the state to set the switch to
-	state = strings.ToLower(state)
-	if state != "on" && state != "off" {
-		apihelpers.EncodeError(w, http.StatusBadRequest, "Invalid State: must be \"on\" or \"off\"")
-		return
-	}
-
-	// Get the code we want to transmit
-	selectedSwitch := switches[intSwitchNum]
-	code := selectedSwitch.On
-	if state == "off" {
-		code = selectedSwitch.Off
-	}
-
-	// Send the code
-	err = rf.SendCode(code)
-
-	// Handle errors from pilight
-	if err != nil {
-		apihelpers.EncodeError(w, http.StatusInternalServerError, "Unable to send RF Code")
-		return
-	}
-	// Success!
-	success := fmt.Sprintf("Successfully turned switch %d %s", intSwitchNum, state)
-	apihelpers.EncodeJSON(w, http.StatusOK, map[string]string{"message": success})
 }
